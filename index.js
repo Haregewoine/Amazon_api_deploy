2ace27632f5fa99f05e61a8e7bfe616c7e62530e
Haregewoine <Haregewoineabire9@gmail.com>
2024-10-17 16:42:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success !",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total
+
+  if (total > 0) {
+    console.log("Payment Request Recieved for this amount >>> ", total);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    res.status(201).json(paymentIntent.client_secret);
+  } else {
+    res.status(403).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+
+app.listen(4242, (err) => {
+  if (err) throw err;
+  console.log(" amazon Server running on port 4242, http://localhost:4242");
+});
+
+
+
+
+
+
